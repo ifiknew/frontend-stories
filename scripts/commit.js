bc11516c0e8f1e98cac2671164118570e40959d2
linzuhua <390162543@qq.com>
2019-04-21 16:43:58 +0800

    feat - add script commit.js

--- scripts/commit.js
+++ scripts/commit.js
@@ -4,15 +4,15 @@ const [ , , commitMessage] = process.argv
 
 console.log('building docz...')
 try {
-  childProcess.execSync('npm run docz:build')
+  childProcess.execSync('npm run docz:build', { stdio: "inherit" })
 } catch (error) {
   console.error('error while building docz:', error)
   process.exit(1)
 }
 console.log('build done, start committing...')
 try {
-  childProcess.execSync('git add .')
-  childProcess.execSync(`git commit -m '${commitMessage}'`)
+  childProcess.execSync('git add .', { stdio: "inherit" })
+  childProcess.execSync(`git commit -m '${commitMessage}'`, { stdio: "inherit" })
 } catch (error) {
   console.error('error when commit:', error)
   process.exit(1)
