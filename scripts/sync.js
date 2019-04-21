66b66b14d02491228cc0f6e1aa8e0e946f2c6ca5
linzuhua <390162543@qq.com>
2019-04-21 22:52:37 +0800

    script - rename commit as sync and add push command

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,22 +1,27 @@
+/**
+ * build docz,
+ * then git add, commit and push
+ */
 const childProcess = require('child_process')
 
 const [ , , commitMessage] = process.argv
 
-console.log('building docz...')
+console.log('building docz')
 try {
   childProcess.execSync('npm run docz:build', { stdio: "inherit" })
 } catch (error) {
   console.error('error while building docz:', error)
   process.exit(1)
 }
-console.log('build done, start committing...')
+console.log('build done, start sync')
 try {
   childProcess.execSync('git add .', { stdio: "inherit" })
   childProcess.execSync(`git commit -m '${commitMessage}'`, { stdio: "inherit" })
+  childProcess.execSync('git push', { stdio: "inherit" })
 } catch (error) {
   console.error('error when commit:', error)
   process.exit(1)
 }
-console.log('commit done.')
+console.log('sync done')
 
 process.exit(0)
