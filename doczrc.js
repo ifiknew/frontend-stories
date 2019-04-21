36822f5c096ebfb3445629884860596a7d4a174b
linzuhua <390162543@qq.com>
2019-04-21 15:06:41 +0800

    fix - spa refersh 404

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,19 @@
+const StaticSiteGenerator = require('webpack-static-site-generator')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
 export default {
   dest: '/docs',
+  indexHtml: path.join(__dirname, '/template/index.ejs'),
+  onCreateWebpackChain: (config) => {
+    return config
+      .plugin('CopyWebpackPlugin')
+      .use(
+        CopyWebpackPlugin,
+        [
+          [
+            { from: path.join(__dirname, '/template/404.html'), to: path.join(__dirname, '/docs/404.html'), toType: 'file' }
+          ]
+        ]
+      )
+  }
 }
